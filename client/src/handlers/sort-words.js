8c4d55b0922315a96666cbe42648d25c4438fe62
Aviv <aviv.dror@gmail.com>
2022-02-16 13:58:48 +0100

    comment

--- client/src/handlers/sort-words.js
+++ client/src/handlers/sort-words.js
@@ -20,7 +20,7 @@ export const sortWords = (event) => {
     sort the words in data.words using the logic function
     assign the newly sorted list to a variable named `sorted`
   */
-  // data.sort = howToSort;
+  // data.sort = howToSort; < -- unnecessary
   const sorted = sortStrings(data.words, howToSort);
   // ... write some code ...
 
